7575289fc45ac827ee4cac29b824b7d376ac33ac
Rohit Nayyar <Rohitnayyar54@gmail.com>
2023-04-19 21:11:09 -0400

    fixed Footer Made testimonial

--- Project1/assets/app.js
+++ Project1/assets/app.js
@@ -1,7 +1,8 @@
 
 window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
-    getInformation();   
+    getInformation(); 
+  
   });
 
 
@@ -40,7 +41,7 @@ function getWorkout(){
 
 function getInformation(){
     document.getElementById("Description").innerHTML="";
-    // document.getElementById("List-Item").innerHTML="";
+
     document.getElementById("Instruction").innerHTML="";
     const selectOption = document.getElementById("select-option");
     const selectedValue = selectOption.value;
@@ -68,37 +69,24 @@ fetch(`https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?name=${enterT
 	.then(function(response){
 var reso = response
 
-        // createH.innerHTML ="";
-        // instruction.innerHTML ="";
-console.log(reso)
-// console.log(reso[0].name)
-//     console.log(reso[0].instructions )
+
         var displayDescr = document.getElementById("Description")
         let createDes = document.createElement("h1")
         createDes.innerHTML = `Showing Results For : ${type}`
         displayDescr.appendChild(createDes);
-        
-        // var intoThis = document.getElementById("List-Item")
-        // response.forEach(element => {
-        //     let createEl = document.createElement("li");
-        //     createEl.className = "exercise"; 
-        //     createEl.innerHTML = element.name;
-        //     intoThis.appendChild(createEl);
-        // });
+    
 
 
     var hereToo = document.getElementById("Instruction");
     reso.forEach(element =>{
         let createH = document.createElement("a");
-        // let image = document.createElement("img");
-        // image.className="Link"
-        // image.src ="./img/external-link.png"
+
         createH.className="Hyper-Link";
         createH.href=`https://www.youtube.com/results?search_query=${element.name} `
         createH.target="_blank";        
         
         createH.innerHTML = `${element.name} &#10162; `;
-        // hereToo.appendChild(image); 
+
         hereToo.appendChild(createH);
 
         let instruction = document.createElement("div");
@@ -148,8 +136,6 @@ fetch('https://ny-times-news-titles-and-urls.p.rapidapi.com/news', newsOptions)
     infoTitle.appendChild(createSport);
 
 
-//    infoTitle.appendChild(createSportTitle);
-
 })
 
 var healthNews = response.health;
@@ -169,8 +155,7 @@ healthNews.forEach(element=>{
 
     createA.appendChild(createImg);
     healthTitle.appendChild(createA);
-        // createA.appendChild(document.createTextNode(element.title));
-        // healthTitle.appendChild(createA);
+        
 
 })  
 var foodNews = response.food;
@@ -210,122 +195,7 @@ foodNews.forEach(element=>{
         
        
     })
-	// .catch(err => console.error(err));
 
 
 
-// var doThis = document.getElementById("Started");
-// doThis.addEventListener("click",function(){
-  
-//     let anotherPage = "./Project1/index.html"
-//     document.location.replace(anotherPage)
-//     getInformation()
-    
-// })
-
-
-
-// function takeMe(getInformation){
-    
-//     document.location.replace(anotherPage);
-    
-// }   
-
-
-
-
-// var techNews = response.tech;
-// var businessNews = response.business;
-// var stories = sportsNews.concat(healthNews).concat(techNews).concat(businessNews);
-// var news = stories.map(element => {
-//  let newsItem = document.createElement("div");
-//  newsItem.className = "news";
-//  newsItem.innerHTML = element.title;
-//  let newsLink = document.createElement("a");
-//  newsLink.className = "news-link";
-//  newsLink.href = element.url;
-//  newsLink.target = "_blank";
-//  newsLink.innerHTML = element.title;
-//  newsItem.appendChild(newsLink);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  var intoThis = document.getElementById("app")
-// response.forEach(element => {
-//     let createEl = document.createElement("div");
-//     createEl.className = "exercise";
-//     createEl.innerHTML = element.name;
-//     intoThis.appendChild(createEl);
-// });
-// let here = document.getElementById("musc")
-//     reso.forEach(element => {
-//         console.log(reso)
-//     let muscle = document.createElement("div");
- 
-//     muscle.className = "muscle";
-//     muscle.innerHTML = element.muscle;
-//     musc.appendChild(muscle);
-   
-
-// });
-
-// var hereToo = document.getElementById("instruction");
-// reso.forEach(element =>{
-//     let instruction = document.createElement("div");
-//     instruction.className = "instruction";
-//     instruction.innerHTML = element.instructions;
-//     hereToo.appendChild(instruction);
-// })
-
-
-   
-
-
-
-
-       
-//         // console.log(response[0].name)
-//     })
-// 	.catch(err => console.error(err));
-
-/*MUSCLE
-abdominals
-abductors
-adductors
-biceps
-calves
-chest
-forearms
-glutes
-hamstrings
-lats
-lower_back
-middle_back
-neck
-quadriceps
-traps
-triceps*/
 
